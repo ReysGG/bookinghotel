ed9340b24079d7384f08eda3c190b395996cd979
ReysGG <davidreysgg@gmail.com>
2025-11-27 23:20:50 +0700

    `Added amenities icons, capacity and price display, and reservation room section to RoomDetail component`

---
+++ app/debug/page.tsx
@@ -0,0 +1,12 @@
+import RoomDetail from "@/components/RoomDetail";
+
+
+const PageDebug = () => {
+  return (
+    <div>
+      <RoomDetail roomID="cmhtgqkzo0000v6gsfjn1ifzk"></RoomDetail>
+    </div>
+  );
+};
+
+export default PageDebug;
---
+++ app/room/[roomId]/page.tsx
@@ -0,0 +1,21 @@
+import RoomDetail from "@/components/RoomDetail";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+    title: "Room detail"
+}
+//{params:Promise<{roomId : string}>}) yang roomId nya harus sama dengan folder
+const DetailPage =async ({params} : {params:Promise<{roomId : string}>}) => {
+    const roomId = (await params).roomId
+  return (
+    <div className="mt-16">
+      <Suspense fallback={<>loading</>}>
+        <RoomDetail roomID={roomId}></RoomDetail>
+      </Suspense>
+      
+    </div>
+  );
+};
+
+export default DetailPage;
